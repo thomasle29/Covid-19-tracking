f1389e557a850d58b8aba21b55914f49d6cd25ce
Quach Dang <quadang21cen@gmail.com>
2021-06-04 19:22:31 +0700

    add new commant

--- core_computing/Demo.py
+++ core_computing/Demo.py
@@ -3,6 +3,7 @@ from Graph_Ex import *
 print("Start...")
 js = read_js("list.json")
 list_obj = to_Graph(js)
+# just for test 
 list_obj[0].filial = 5
 list_obj[0].edge[0].filial = 10
 for obj in list_obj:
